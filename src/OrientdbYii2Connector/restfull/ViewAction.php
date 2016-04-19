9dd90cc492dd60338779a1e3dd38262f8912b62a
haiflive <jak_67@mail.ru>
2016-04-19 20:14:51 +1000

    Add restfull find by pk

--- src/OrientdbYii2Connector/restfull/ViewAction.php
+++ src/OrientdbYii2Connector/restfull/ViewAction.php
@@ -14,7 +14,7 @@ class ViewAction extends Action
      */
     public function run($id)
     {
-        $model = $this->findModel($id);
+        $model = $this->findModel('#'.$id);
         if ($this->checkAccess) {
             call_user_func($this->checkAccess, $this->id, $model);
         }
