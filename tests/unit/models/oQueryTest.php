4cce81e8d0d2aa3a1f5e57744a16619e7ecd7bb6
haiflive <jak_67@mail.ru>
2017-07-11 22:52:30 +1000

    Add Query realtion tests

--- tests/unit/models/oQueryTest.php
+++ tests/unit/models/oQueryTest.php
@@ -4,11 +4,10 @@ namespace tests\codeception\unit\models;
 
 use Yii;
 use OrientDBYii2Connector\Query;
-use OrientDBYii2Connector\DataRreaderOrientDB;
 
 /**
  *  example run single test
- *  > codecept run unit models/oDealTest:testCreatePrice --debug
+ *  > codecept run unit models/oQueryTest:testCreateRecord --debug
  *  debugging:
  *  codecept_debug($post);
  */
@@ -40,6 +39,88 @@ class oQueryTest extends \Codeception\Test\Unit
         
         $this->assertTrue(count($test_query_all) > 0, 'test query all');
         $this->assertTrue($test_query_all[0]['@rid'] == $good_insert['@rid'] , 'test query all');
+    }
+    
+    public function testCreateRelatedEmbedded()
+    {
+        $client = Yii::$app->dborient->createCommand();
+        
+        $expense_insert = $client->insert('Expense', [
+            'Name' => 'test Query Expense related',
+            'Price' => '1021',
+            // 'executor' => // linlk
+            'prices' => [[ // embedded list (has_many)
+              // "@type" => "d",
+              // "@version" => 0,
+              "@class" => "Price", //! required for embedded
+              "Price" => 45,
+              "Cost" => 156,
+              "transport" => [ // embedded (has_one)
+                "@class" => "Transport", //! required for embedded
+                "Capacity" => 3,
+                "ContainerKind" => "test Query Expense embedded -> embedded"
+              ]
+            ]]
+        ])->execute();
+        
+        // get by RID
+        $expense_select = (new Query())->from('Expense')
+          ->where(['@rid'=>$expense_insert['@rid']])
+          ->one();
+        
+        $this->assertTrue($expense_insert['@rid'] == $expense_select['@rid'], 'compare RID, just insert and select');
+        $this->assertTrue($expense_insert['Name'] == $expense_select['Name'], 'compare Name, just insert and select');
+        $this->assertTrue($expense_insert['Price'] == $expense_select['Price'], 'compare Price, just insert and select');
+        $this->assertTrue($expense_insert['prices'][0]['Cost'] == $expense_select['prices'][0]['Cost'], 'compare prices(embedded), just insert and select');
+        $this->assertTrue($expense_insert['prices'][0]['transport']['ContainerKind'] == $expense_select['prices'][0]['transport']['ContainerKind'], 'compare prices(embedded -> embedded), just insert and select');
+        
+        $test_query_all = (new Query())
+          ->from('Expense')
+          ->where(['@rid'=>$expense_insert['@rid']])
+          ->all();
+        
+        $this->assertTrue(count($test_query_all) > 0, 'test query all');
+        $this->assertTrue($test_query_all[0]['@rid'] == $expense_insert['@rid'] , 'test query all');
+    }
+    
+    public function testCreateRelatedLink()
+    {
+        $client = Yii::$app->dborient->createCommand();
+        
+        // create record `Organization` for relation with `Expense`
+        $organization_insert = $client->insert('Organization', [
+            'Country' => 56,
+            'OrganizationName' => 'rest link realtion organization name',
+            'Site' => 'test.te'
+        ])->execute();
+        
+        // create `Expense` with link relation `executor`
+        $expense_insert = $client->insert('Expense', [
+            'Name' => 'test Query Expense related',
+            'Price' => '1021',
+            'executor' => $organization_insert['@rid'] // linlk
+        ])->execute();
+        
+        // get by RID
+        $expense_select = (new Query())->from('Expense')
+          ->where(['@rid'=>$expense_insert['@rid']])
+          ->fetch_plan('executor:0') // `:0` - is level (see more documentation OrientDB fetch_plan)
+          ->one();
+        
+        $this->assertTrue($expense_insert['@rid'] == $expense_select['@rid'], 'compare RID, just insert and select');
+        $this->assertTrue($expense_insert['Name'] == $expense_select['Name'], 'compare Name, just insert and select');
+        $this->assertTrue($expense_insert['Price'] == $expense_select['Price'], 'compare Price, just insert and select');
+        
+        $this->assertTrue($expense_select['executor']['Country'] == $organization_insert['Country'], 'compare Country link relation executor, just insert and select');
+        $this->assertTrue($expense_select['executor']['OrganizationName'] == $organization_insert['OrganizationName'], 'compare OrganizationName link relation executor, just insert and select');
+        
+        $test_query_all = (new Query())
+          ->from('Expense')
+          ->where(['@rid'=>$expense_insert['@rid']])
+          ->all();
+        
+        $this->assertTrue(count($test_query_all) > 0, 'test query all');
+        $this->assertTrue($test_query_all[0]['@rid'] == $expense_insert['@rid'] , 'test query all');
     }
     
     
